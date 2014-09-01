5077ec428c591f4208308aa2541b5a3c6c53fcd0
root <root@iguatu.cenapad.ufc.br>
2014-09-01 14:51:25 -0300

    quick fix o db creation script

--- app_assets/db_creation.sql
+++ app_assets/db_creation.sql
@@ -1,11 +1,11 @@
-create database 'a2cc';
+create database a2cc;
 
-use 'a2cc';
-create table 'opennebula_allocated_vms' (
+use a2cc;
+create table opennebula_allocated_vms (
 	username varchar(20),
 	vmid int,
 	primary key (username, vmid)
 );
 
-create user 'a2cc' identified by '1!2@3#';
-grant select, insert, delete on a2cc.opennebula_allocated_vms to 'a2cc';
+create user 'a2cc'@'localhost' identified by '1!2@3#';
+grant select, insert, delete on a2cc.* to 'a2cc'@'localhost';
