287c25e63d92b0ac1b414c4ef7ffe04b1e87cf56
RenatoCJN <renatocjn@gmail.com>
2015-02-26 15:16:29 -0300

    new login.php layout; Introduction on login.php

--- en/login.php
+++ en/login.php
@@ -25,16 +25,18 @@
 			include 'banner.php';
 		?>
 		
-		<div class="conteudo grid_14">		
-		
-		<?php
-			if (isset($_SESSION['notice']) ) {
-				print '<div class="notice"> '.$_SESSION['notice'].' </div>';
-				unset($_SESSION['notice']);
-			}
-		?>
+		<div class="conteudo grid_14">
 			<form id="form1" name="form1" class="validate formLogin" method="post" onsubmit="return sem_branco(['campo_login', 'campo_senha'])" action="../valida3.php">
 				<table>
+					<h4> Access to the application portals </h4>
+					<tr>
+						<?php
+							if (isset($_SESSION['notice']) ) {
+								print '<div class="notice"> '.$_SESSION['notice'].' </div>';
+								unset($_SESSION['notice']);
+							}
+						?>
+					</tr>
 					<tr>
 						<td>User Name</td>
 						<td><input type="text" class="" id="usuario" name="usuario" /></td>
@@ -52,6 +54,11 @@
 					</tr>
 				</table>
 			</form>
+			<p> The <abbr title="Centro Nacional de Processamento de Alto Desempenho, National Center of High Performance Processing">CENAPAD</abbr> of the <abbr title="UFC">Federal University of Ceará</abbr>, subjected to the <abbr title="Pró-reitoria de planejamento">Planning directory</abbr>, has the objective of providing High Performance Processing (HPP) Services for the demand of universities, research institutes and other public and private institutions. </p>
+			<p> CENAPAD is part of the <abbr title="Sistema Nacional de Processamento de Alto Desempenho, National System of High performance processing">SINAPAD</abbr>, a network of geographically distributed HPP providers with the objective of scientific and technological improvements for the country. </p>
+			<p> These portals have the objective of providing easy access to the <a href="http://www.cenapad.ufc.br/quem-somos/recursos"> computational resources available at CENAPAD - UFC. </a> Regular access to these resource require special training to directly access them. </p>
+			<p> To connect and run applications on the cluster, users need training on Linux shell and SLURM commands. To use a cloud, users need to request virtual machines and learn Linux shell. With this portal, any user can use the resources through this simple web interface  where the portals decide which is the better architecture to run each application and executes the required commands to run the desired application. </p>
+			<p> Further Information and requests for credentials must be requested via e-mail: <a href="mailto:suporte@cenapad.ufc.br"> suporte@cenapad.ufc.br </a> </p>  
 		</div>
 		<?php include 'navbar.php'; ?>
 		
--- pt/login.php
+++ pt/login.php
@@ -27,15 +27,17 @@
 		?>
 		
 		<div class="conteudo grid_14">
-		
-		<?php
-			if (isset($_SESSION['notice']) ) {
-				print '<div class="notice"> '.$_SESSION['notice'].' </div>';
-				unset($_SESSION['notice']);
-			}
-		?>
 			<form id="form1" name="form1" class="validate formLogin" method="post" onsubmit="return sem_branco(['campo_login', 'campo_senha'])" action="../valida3.php">
 				<table>
+					<h4> Acesso aos portais de aplicação </h4>
+					<tr>
+						<?php
+							if (isset($_SESSION['notice']) ) {
+								print '<div class="notice"> '.$_SESSION['notice'].' </div>';
+								unset($_SESSION['notice']);
+							}
+						?>
+					</tr>
 					<tr>
 						<td>Nome</td>
 						<td><input type="text" class="" id="usuario" name="usuario" /></td>
@@ -53,6 +55,11 @@
 					</tr>
 				</table>
 			</form>
+			<p> O <b>Centro Nacional de Processamento de Alto Desempenho (CENAPAD)</b> da Universidade Federal do Ceará (UFC), ligado à Pró-Reitoria de Planejamento, tem por objetivo prestar serviços de Processamento de Alto Desempenho (PAD) sob demanda às universidades, institutos de pesquisa e outras instituições publicas e privadas. </p>
+			<p> Ele é integrante do consórcio SINAPAD (Sistema Nacional de Processamento de Alto Desempenho), uma rede de centros geograficamente espalhados pelo Brasil com o intuito de fornecer os recursos de PAD necessário para o desenvolvimento científico e tecnológico do país.  </p>
+			<p> Estes portais tem como objetivo facilitar o acesso aos <a href="http://www.cenapad.ufc.br/quem-somos/recursos"> recursos disponíveis no CENAPAD - UFC </a> devido à necessidade de treinamento especifico para tratar diretamente com eles.</p>
+			<p> Para tratar com o cluster o usuário precisa de treinamento com Linux shell e comandos SLURM. Para tratar com a cloud o	usuário precisa requisitar máquinas virtuais e aprender Linux shell. Com a utilização deste portal o usuário pode utilizar estes recursos através desta interface web simples onde o portal determina em qual infraestrutura ele executará a aplicação e executa os comandos necessários. </p>
+			<p> Requisitos de credenciais, duvidas e mais informações podem ser requisitadas via e-mail: <a href="mailto:suporte@cenapad.ufc.br"> suporte@cenapad.ufc.br </a>  </p>
 		</div>
 		
 		<?php include 'navbar.php'; ?>
